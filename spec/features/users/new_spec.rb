46b4431892fe7a63b21d89c1085c7cc1f834a031
Hoa Dam <hoangocdam@gmail.com>
2024-04-11 09:49:19 -0700

    feat:test for signup with Google

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Log in', type: :feature do
+  describe 'When user visits sign up page' do
+    it "users can sign up using their google acocunt" do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+        provider: 'google',
+        uid: '123456',
+        info: {
+          name: 'John Doe',
+          email: 'test@example.com'
+        }
+      })
+
+      visit register_user_path
+      click_on "Sign Up with Google"
+
+      expect(current_path).to eq(user_path(User.first))
+    end
+  end
+end
