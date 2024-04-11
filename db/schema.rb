4699e57c17f127670bacb644b354cd9e4ba2e187
Hoa Dam <hoangocdam@gmail.com>
2024-04-11 08:54:28 -0700

    change username to name and delete location on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_221523) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_034000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.string "location"
   end
 
 end
