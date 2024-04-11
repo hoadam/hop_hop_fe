02df312ab922b4b9bc0c34eba551145194501eae
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-04-11 16:09:28 -0600

    Test: sign up happy and sad path

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe 'Sign Up', type: :feature do
-  describe 'When user visits sign up page' do
+RSpec.describe 'User Registration', type: :feature do
+    it 'shows that user signed up successfully' do
+        visit register_user_path
+
+        fill_in 'Name', with: 'Selena'
+        fill_in 'Email', with: 'selena@gmail.com'
+        fill_in 'Password:', with: 'selena123'
+        fill_in 'Password Confirmation:', with: 'selena123'
+        
+        click_button 'Create New User'
+
+        expect(current_path).to eq(user_path(User.find_by(email: 'selena@gmail.com')))
+    end
+
+    it 'returns error message for wrong credentials' do #Sad path
+        visit register_user_path
+
+        fill_in 'Name', with: 'Selena'
+        fill_in 'Email', with: ''
+        fill_in 'Password:', with: 'selena123'
+        fill_in 'Password Confirmation:', with: '123'
+        
+        click_button 'Create New User'
+
+        expect(page).to have_content("Email can't be blank")
+        expect(page).to have_content("Password confirmation doesn't match Password")
+        expect(current_path).to eq(register_user_path)
+    end
+    
     it "users can sign up using their google acocunt" do
       OmniAuth.config.test_mode = true
       OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
@@ -12,11 +39,13 @@ RSpec.describe 'Sign Up', type: :feature do
           email: 'test@example.com'
         }
       })
-
+    
       visit register_user_path
       click_on "Sign Up with Google"
-
+    
       expect(current_path).to eq(user_path(User.first))
     end
-  end
 end
+
+
+
