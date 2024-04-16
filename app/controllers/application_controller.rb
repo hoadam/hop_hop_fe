e305875a235505e7e97256d41911d97de66b6663
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-16 00:33:12 -0400

    feat:add devise to migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
 
+  private
+
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  private
+  def authorize
+    redirect_to user_login_path, :notice => "Not Authorized" if current_user.nil?
+  end
 
   def error_message(errors)
     errors.full_messages.join(', ')
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+      passwords: 'passwords',
+      registrations: 'registrations',
+      sessions: 'sessions'
+  }
+  resource :two_factor_settings, except: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
