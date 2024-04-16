c5edcdc2df87bd366b19af50e9d54a0fe3d67833
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-15 17:52:25 -0700

    Revert "feat: Add API key to credentials and create Discover button for Nav"
    This reverts commit ebda1cc2d1235afe6aff15ebc609466cf14e01b5.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "mapkick/bundle"
 
 document.addEventListener('turbolinks:load', function() {
   let map;
--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,40 +1,40 @@
-let map;
+// let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
 
-initMap();
+// initMap();
 
-async function initMap() {
-  // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    "marker",
-  );
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: { lat: -25.363882, lng: 131.044922 },
-    mapId: "DEMO_MAP_ID",
-  });
+// async function initMap() {
+//   // Request needed libraries.
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+//     "marker",
+//   );
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: { lat: -25.363882, lng: 131.044922 },
+//     mapId: "DEMO_MAP_ID",
+//   });
 
-  map.addListener("click", (e) => {
-    console.log("Click")
-    placeMarkerAndPanTo(e.latLng, map);
-  });
-}
+//   map.addListener("click", (e) => {
+//     console.log("Click")
+//     placeMarkerAndPanTo(e.latLng, map);
+//   });
+// }
 
-function placeMarkerAndPanTo(latLng, map) {
-  new google.maps.marker.AdvancedMarkerElement({
-    position: latLng,
-    map: map,
-  });
-  map.panTo(latLng);
-}
+// function placeMarkerAndPanTo(latLng, map) {
+//   new google.maps.marker.AdvancedMarkerElement({
+//     position: latLng,
+//     map: map,
+//   });
+//   map.panTo(latLng);
+// }
 
-initMap();
+// initMap();
