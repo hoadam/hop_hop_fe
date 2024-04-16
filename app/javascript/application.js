2c0d94f226f10aa2e3ec1a34bc1379f3afe353ba
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-16 16:47:09 -0700

    fix: Reorder code and add comments

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,122 +1,78 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-  // Option 1: decouple and call init with a utility file
-  // Option 2: Mutation observer, any time something changes, initiate the map with the params
-  // Find a way to re-render with the partial
-/**
- * Functions -----------
- */
 
-const waitForElement = async function(elem) {
-  if (typeof  elem  ==  'string') {
-      return  new Promise(function (resolve) {
-          var  wfelem  =  function () {
-              if (null  !=  document.querySelector(elem)) {
-                  resolve(document.querySelector(elem));
-              } else {
-                  window.requestAnimationFrame(wfelem);
-              }
-          };
-          wfelem();
-      });
-  }
-};
+// Declare the map variable
+let map
 
-async function addMO (selector) {
-        // Select the node that will be observed for mutations
-        const node = await waitForElement(selector)
+// Create an asynchronous function to initialize the map
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
 
-        // Options for the observer (which mutations to observe)
-        const config = { childList: true};
-      
-        // Callback function to execute when mutations are observed
-        const callback = (mutationList, observer) => {
-          for (const mutation of mutationList) {
-            if (mutation.type === "childList") {
-              console.log("A child node has been added or removed.", {mutationList});
-              // where you would look for the map being initiated or not and re-init
-            } else console.log(mutationList)
-          }
-        };
-      
-        // Create an observer instance linked to the callback function
-        const observer = new MutationObserver(callback);
-      
-        // Start observing the target node for configured mutations
-        observer.observe(node, config);
-      
-        // // Later, you can stop observing
-        // observer.disconnect();
+  // map would be the return value of the pulling function looking for the map div
+  // map = a function that keeps looking for the value, and then assigns it to map
+  map = new Map(await waitForElement("#map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+
+  map.addListener("click", (e) => {
+    map.panTo(e.latLng);
+  });
+  // Adding a pin to search results
+  // Access the data from the data-info attribute of your element
+  const dataArray = document.getElementById("searchResults").dataset.info;
+  console.log(dataArray);
+  // Parse the data to extract the lat and lon
+  const jsonData = JSON.parse(dataArray);
+  console.log(jsonData);
+
+  jsonData.forEach(location => {
+    const latitude = location.data.lat;
+    const longitude = location.data.lon;
+
+    const latLng = new google.maps.LatLng(latitude, longitude);
+
+    placeMarker(latLng, map)
+    map.panTo(latLng);
+  });
 }
 
+document.addEventListener('turbo:load', function() {
+  initMap(); // Call your function to initialize Google Maps
+});
 
-addMO("#map")
-
-  let map 
-
-  // document.querySelector("#map");
-  // console.log("\n\n", {map})
-
-  async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-    // map would be the return value of the pulling function looking for the map div
-    // map = a function that keeps looking for the value, and then assigns it to map
-
-    // map = await waitForElement("#map")
-
-    map = new Map(await waitForElement("#map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-
-    console.log("\n\n",  {map})
-
-
-    // Access the data from the data-info attribute of your element
-
-    console.log(document.getElementById("searchResults"))
-    const dataArray = document.getElementById("searchResults").dataset.info;
-    console.log(dataArray);
-    // Parse the data to extract the lat and lon
-    const jsonData = JSON.parse(dataArray);
-
-    console.log(jsonData);
-    jsonData.forEach(location => {
-      const latitude = location.data.lat;
-      const longitude = location.data.lon;
-
-      const latLng = new google.maps.LatLng(latitude, longitude);
-
-      placeMarker(latLng, map)
-      map.panTo(latLng);
-    });
-  }
-
-  function placeMarker(latLng, map) {
+// Place markers
+  const placeMarker = function(latLng, map) {
     new google.maps.Marker({
       position: latLng,
       map: map,
     });
   }
 
-  initMap();
 
+  // Used to wait for the #map to appear, if it does, is it a String?
+  // If it is a String, then return a promise
+  const waitForElement = async function(elem) {
+    console.log(elem)
+    if (typeof  elem  ===  'string') {
+      // Promise waits for the result of a function and will run as soon as it can?
+        return  new Promise(function (resolve) {
+          // declare a new function
+            var  wfelem  =  function () {
+              // query the document for css selector of #map
+                if (null  !=  document.querySelector(elem)) {
+                    resolve(document.querySelector(elem));
+                } else {
+                    window.requestAnimationFrame(wfelem);
+                }
+            };
+            wfelem();
+        });
+    }
+  };
 
-
-    // Place markers for each set of coordinates in the array
-
-
-  // Function to place a marker at a specific location on the map
-
-
-  // map.addListener("click", (e) => {
-  //   placeMarkerAndPanTo(e.latLng, map);
-  // });
-
-
-
-  // async function getPlaceDetails(Place) {
+  // const getPlaceDetails = async function(Place) {
   //   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   //   // Use place ID to create a new Place instance.
   //   const place = new Place({
@@ -127,9 +83,6 @@ addMO("#map")
   //   await place.fetchFields({
   //     fields: ["displayName", "formattedAddress", "location"],
   //   });
-  //   // Log the result
-  //   console.log(place.displayName);
-  //   console.log(place.formattedAddress);
 
   //   // Add an Advanced Marker
   //   const marker = new AdvancedMarkerElement({
@@ -138,31 +91,3 @@ addMO("#map")
   //     title: place.displayName,
   //   });
   // }
-
-// Initialize and add the map
-// let map;
-
-// async function initMap() {
-//   // The location of Uluru
-//   const position = { lat: -25.344, lng: 131.031 };
-//   // Request needed libraries.
-//   //@ts-ignore
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-//   // The map, centered at Uluru
-//   map = new Map(document.getElementById("map"), {
-//     zoom: 4,
-//     center: position,
-//     mapId: "DEMO_MAP_ID",
-//   });
-
-//   // The marker, positioned at Uluru
-//   const marker = new AdvancedMarkerElement({
-//     map: map,
-//     position: position,
-//     title: "Uluru",
-//   });
-// }
-
-// initMap();
