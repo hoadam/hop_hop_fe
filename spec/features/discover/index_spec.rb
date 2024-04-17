b80daaaa32ece42d890d3d2bcccfb7669843f6b3
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-16 21:23:05 -0700

    feat: Add partials to render forms

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe "Discover Index", type: :feature do
+  context "a user searches for a location" do
+    it "displays the results with clickable links" do
+      within "#-#{@.id}" do 
+      
+      end 
+      expect(page).to have_content
+    end
+  end
+end
