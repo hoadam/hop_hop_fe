758d250156370a56f12bd51cde3db0f447fed226
Hoa Dam <hoangocdam@gmail.com>
2024-04-16 18:46:34 -0700

    feat:create and destroy methods

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,10 +1,14 @@
 class AccommodationsController < ApplicationController
   def new
-
+    @user = User.find(params[:user_id])
+    @trip = TripService.trip_details(params[:user_id], params[:trip_id])
   end
 
   def create
-
+    @user = User.find(params[:user_id])
+    @trip = TripService.trip_details(params[:user_id], params[:trip_id])
+    @accommodation = AccommodationService.create_accommodation(params[:user_id], params[:trip_id], accommodation_params)
+    redirect_to user_trip_path(@user, @trip.id)
   end
 
   def edit
@@ -23,6 +27,17 @@ class AccommodationsController < ApplicationController
   rescue => e
     flash[:error] = "Failed to update accommodation - #{e.message}"
     redirect_to edit_user_trip_accommodation_path(@user, @trip.id, @accommodation.id)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @trip = TripService.trip_details(params[:user_id], params[:trip_id])
+    @accommodation = AccommodationService.accommodation_details(params[:user_id], params[:trip_id], params[:id])
+    AccommodationService.delete_accommodation(params[:user_id], params[:trip_id], params[:id])
+    redirect_to user_trip_path(@user, @trip.id)
+  rescue => e
+    flash[:error] = "Failed to delete accommodation - #{e.message}"
+    redirect_to user_trip_path(@user, @trip.id)
   end
 
   private
