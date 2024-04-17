63e15c85bac7de61532a73387fdb922d9c3a3d4b
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-16 21:08:35 -0700

    test: Add and reform testing for DiscoverFacade

--- spec/facades/discover_facade_spec.rb
+++ spec/facades/discover_facade_spec.rb
@@ -1,18 +1,16 @@
 require "rails_helper"
 
 RSpec.describe DiscoverFacade do
-  before(:each) do
+  let(:search) {DiscoverFacade.new("Disneyland")}
 
-  end
-
-  describe "instance methods" do
-    let(:search) {DiscoverFacade.new("Disneyland")}
-
+  describe "#initialize" do
     it "initializes with search parameters" do
       expect(search).to be_a DiscoverFacade
       expect(search.search_params).to eq("Disneyland")
     end
+  end
 
+  describe "#find_location" do
     it "can find locations of the search parameter", :vcr do
       locations = search.find_location
       expect(locations).to be_an(Array)
@@ -20,7 +18,9 @@ RSpec.describe DiscoverFacade do
         expect(location.data).to be_a(Hash)
       end
     end
+  end
 
+  describe "#search_objects" do
     it "can turn the search results into Search objects", :vcr do
       search_objects = search.search_objects
 
@@ -30,23 +30,31 @@ RSpec.describe DiscoverFacade do
         expect(object).to be_a(Search)
       end
     end
+  end
 
+  describe "#json_data" do
     it "can turn search results into json data", :vcr do
       json_data = search.json_data
 
       expect(json_data).to be_a(String)
     end
+  end
 
+  describe "#filtered_search_objects" do
     let(:paris) { DiscoverFacade.new("Paris")}
+
     it "can filter the created Search objects and select only unique display names", :vcr do
       expect(paris.search_objects.count).to eq(5)
       expect(paris.filtered_search_objects.count).to eq(3)
     end
+  end
 
+  describe "#empty?" do
     let(:big_air_chandler) { DiscoverFacade.new("Big Air Chandler")}
+
     it "checks if an array of objects is empty", :vcr do
       expect(big_air_chandler.empty?).to eq(true)
-      expect(paris.empty?).to eq(false)
+      expect(search.empty?).to eq(false)
     end
   end
 end
