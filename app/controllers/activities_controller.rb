c6e9961d573bb359a99dcb96cba0e7f4de348f11
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-18 12:16:10 -0700

    feat: Send to can_turn_the_search_results_into_Search_objects

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -20,6 +20,7 @@ class ActivitiesController < ApplicationController
     @activity = ActivityService.activity_details(current_user.id, params[:trip_id], params[:daily_itinerary_id], params[:id])
 
     ActivityService.update_activity(current_user.id, params[:trip_id], params[:daily_itinerary_id], params[:id], activity_params)
+    redirect_to trip_path(@trip.id)
   rescue => e
     flash[:error] = "Failed to update activity - #{e.message}"
     redirect_to edit_trip_daily_itinerary_activity_path(current_user.id, params[:trip_id], params[:daily_itinerary_id], params[:id])
