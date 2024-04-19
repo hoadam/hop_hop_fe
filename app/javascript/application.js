19939667517be0778e820fe4d8bdf7a13734f236
Hoa Dam <hoangocdam@gmail.com>
2024-04-19 09:27:57 -0700

    add:condition statment for maps api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
-
+if (!document.getElementById("trip-show")) {
 // Declare the map variable)
 let map
 
@@ -66,4 +66,4 @@ document.addEventListener('turbo:load', function() {
         });
     }
   };
- w
+}
